4d4cc24ea7611ba4ef7104c2e37b9fbca8c67233
Aoihito <Kimukimu524@icloud.com>
2023-07-16 15:35:52 +0900

    Initial Commit

---
+++ Compact Schedule/Compact_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compact_ScheduleApp.swift
+//  Compact Schedule
+//
+//  Created by 木村友祐 on 2023/07/16.
+//
+
+import SwiftUI
+
+@main
+struct Compact_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Compact Schedule/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Compact Schedule
+//
+//  Created by 木村友祐 on 2023/07/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
